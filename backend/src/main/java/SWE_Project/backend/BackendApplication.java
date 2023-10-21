4de6a03a6cf316ad1fbf58963d2b3f2838b18aa5
sung-yeop <jjabc9459@naver.com>
2023-10-21 18:55:51 +0900

    프로젝트 생성

--- backend/src/main/java/SWE_Project/backend/BackendApplication.java
+++ backend/src/main/java/SWE_Project/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.sw.project;
+package SWE_Project.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/SWE_Project/backend/BackendApplicationTests.java
+++ backend/src/test/java/SWE_Project/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sw.project;
+package SWE_Project.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
