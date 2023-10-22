5e0c6e39a1cf7e77a645b00ed1c310b99b48fa51
sung-yeop <jjabc9459@naver.com>
2023-10-22 21:33:18 +0900

    API (POST) 구현 및 Vector 수정

--- backend/src/main/java/SWE_Project/backend/BackendApplication.java
+++ backend/src/main/java/SWE_Project/backend/BackendApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
