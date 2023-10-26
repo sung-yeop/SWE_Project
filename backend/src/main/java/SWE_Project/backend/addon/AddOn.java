58f7269abc4c0d4d06926f19c0dcdae07caa7fd6
sung-yeop <jjabc9459@naver.com>
2023-10-26 15:51:06 +0900

    API 구현 및 UML 수정

--- backend/src/main/java/SWE_Project/backend/addon/AddOn.java
+++ backend/src/main/java/SWE_Project/backend/addon/AddOn.java
@@ -2,10 +2,8 @@ package SWE_Project.backend.addon;
 
 import SWE_Project.backend.common.Vector;
 import SWE_Project.backend.map.Map;
-import SWE_Project.backend.map.MapController;
 import SWE_Project.backend.movement.RobotMovementInterface;
 import SWE_Project.backend.sensor.SensorController;
-import SWE_Project.backend.sensor.SensorResult;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -15,27 +13,26 @@ import java.util.List;
 public class AddOn {
 
     private RobotMovementInterface robotMovementInterface = new RobotMovementInterface();
-    private MapController mapController = Map.getInstance();
-    private SensorController sensorController = new SensorController(mapController.getPosition(), robotMovementInterface.getRobot().getDirection());
+    private SensorController sensorController = new SensorController();
 
-
-    // 위에서 제공된 mapController 객체를 통해 길찾기 로직을 진행하여 경로를 확인한다.
-    public List<Vector> pathFinding() {
+    //
+    public List<Vector> pathFinding(Map map) {
         List<Vector> path = new ArrayList<>();
 
-        AStar aStar = new AStar(mapController.getSize(), mapController.createMapInit());
+        AStar aStar = new AStar(map.getSize(), map.createMapInit());
 
-        for (Vector vector : mapController.getSpotList()) {
-            ArrayList<Vector> search = aStar.search(sensorController.getPositioningSensor().getPosition(), vector,
-                    mapController.getHazardList());
+        Vector current = sensorController.getPositioningSensor().getPosition();
+
+        for (Vector spot : map.getSpotList()) {
+            ArrayList<Vector> search = aStar.search(current, spot, map.getHazardList());
             for (Vector v : search) {
-                path.add(v);
+                if (!v.equals(spot)) {
+                    path.add(v);
+                }
             }
+            current = spot;
         }
-
-        // 미리 알고 있는 스팟들을 경유하면서 길을 찾는 알고리즘
-        // 스팟 사이의 가중치를 만들고 가장 가까운 스팟 사이를 경유하는 과정을 만들어야 할 듯
-        // 내부 메서드에서는 만약, 스팟을 찾아가는 과정에서 스팟을 밟고 지나간다면 해당 함수를 종료시키고 다시 스팟 찾는 알고리즘을 돌려야함
+        path.add(current);
 
         return path;
     }
@@ -44,23 +41,23 @@ public class AddOn {
     // 이동 도중, 10% 확률로 2칸 이동 등의 오작동을 대비해야할 필요가 있다.
     // 추가적으로 숨겨진 스팟에 대한 내용도 한번에 처리하기 위해 매번 센서를 작동시킨다.
     // 경로를 인풋받아서 이동한다.
-    public Vector find(List<Vector> path) {
-        // path를 이용하여 경로 설정
-        // path에 나온 값과 현재 포지션의 값을 빼면서 방향 전환
-        // positioningSensor.updateDirection(방향);
-        // 전방이 HAZARD가 아닌지 확인
-        if (sensorController.sensor(robotMovementInterface.getRobot().getDirection(), mapController.getHazardList(),
-                mapController.getSpotList()) != SensorResult.HAZARD) {
-
-        }
-        // 앞으로 이동 (앞으로 이동한 후, 해당 포지션 값 반환 필요)
-        // 반환된 값과 위에서 확인한 path를 positioningSensor를 이용하여 정상 이동 확인
-        // Boolean flag = positioningSensor.updatePosition(위에서 반환된 값, path.get(0));
-        // 만약 정상이동이면 이동한 데이터를 Map -> MapController 저장 -> API -> Front로 데이터를 넘기면서 이동 화면에 표시
-        // <-> flag == flase이면 경로 재설정
-
-        return new Vector(0, 0);
-    }
+//    public Vector find(List<Vector> path) {
+//        // path를 이용하여 경로 설정
+//        // path에 나온 값과 현재 포지션의 값을 빼면서 방향 전환
+//        // positioningSensor.updateDirection(방향);
+//        // 전방이 HAZARD가 아닌지 확인
+//        if (sensorController.sensor(robotMovementInterface.getRobot().getDirection(), controller.getMap().getHazardList(),
+//                controller.getMap().getSpotList()) != SensorResult.HAZARD) {
+//
+//        }
+//        // 앞으로 이동 (앞으로 이동한 후, 해당 포지션 값 반환 필요)
+//        // 반환된 값과 위에서 확인한 path를 positioningSensor를 이용하여 정상 이동 확인
+//        // Boolean flag = positioningSensor.updatePosition(위에서 반환된 값, path.get(0));
+//        // 만약 정상이동이면 이동한 데이터를 Map -> MapController 저장 -> API -> Front로 데이터를 넘기면서 이동 화면에 표시
+//        // <-> flag == flase이면 경로 재설정
+//
+//        return new Vector(0, 0);
+//    }
 
     // 센서들의 작동을 묶어서 결과를 리턴하는 메서드
 
--- backend/src/main/java/SWE_Project/backend/map/Map.java
+++ backend/src/main/java/SWE_Project/backend/map/Map.java
@@ -1,41 +1,66 @@
 package SWE_Project.backend.map;
 
 import SWE_Project.backend.common.Vector;
-import java.util.List;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 @Getter
+@Setter
 public class Map {
-    private static MapController mapController;
+    private Vector size;
+    private Vector startSpot;
+    private List<Vector> hazardList = new ArrayList<>();
+    private List<Vector> spotList = new ArrayList<>();
 
     // 초기 지도 생성
-    public Map(Vector size, Vector startPoint, List<Vector> hazardList, List<Vector> spotList) {
-        mapController.setSize(size);
-        mapController.setStartSpot(startPoint);
-        mapController.setHazardList(hazardList);
-        mapController.setSpotList(spotList);
+
+    public Queue<Vector> createMap() {
+        Queue<Vector> result = new LinkedList<>();
+        for (int i = 0; i < size.getY(); i++) {
+            for (int j = 0; j < size.getX(); j++) {
+                result.add(new Vector(j, i));
+            }
+        }
+        return result;
     }
 
-    public static synchronized MapController getInstance() {
-        if (mapController == null) {
-            return mapController = new MapController();
-        } else {
-            return mapController;
+    public Vector[][] createMapInit() {
+        Vector[][] result = new Vector[size.getX()][size.getY()];
+
+        Queue<Vector> mapqueue = createMap();
+
+        for (int i = 0; i < size.getY(); i++) {
+            for (int j = 0; j < size.getX(); j++) {
+                result[j][i] = mapqueue.poll();
+            }
         }
+
+        return result;
     }
 
     // 새로운 데이터는 어떻게 다룰것인지? // 새로운 리스트 생성?
     public void addHazard(Vector... hazard) {
         for (Vector vector : hazard) {
-            mapController.getHazardList().add(vector);
+            getHazardList().add(vector);
         }
     }
 
     public void addSpot(Vector... Spot) {
         for (Vector vector : Spot) {
-            mapController.getSpotList().add(vector);
+            getSpotList().add(vector);
         }
     }
 
 
+    public void init(Vector size, Vector startPoint, List<Vector> spotList, List<Vector> hazardList) {
+        this.size = size;
+        this.startSpot = startPoint;
+        this.spotList = spotList;
+        this.hazardList = hazardList;
+    }
 }
