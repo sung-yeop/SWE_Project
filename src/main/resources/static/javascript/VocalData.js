2ef763b8ada87e744103ac121a35b2f8517c4088
sung-yeop <jjabc9459@naver.com>
2023-11-30 00:37:53 +0900

    refactor(VocalData): VocalData 로직 수정
    - VocalData 로직 수정

--- src/main/resources/static/javascript/VocalData.js
+++ src/main/resources/static/javascript/VocalData.js
@@ -21,25 +21,12 @@ function startRecording() {
 //stopRecording*********************************************************************************
 function stopRecording() {
     recognition.stop();
-<<<<<<< Updated upstream
-    // const textArr = vocalText.split(" ");
 
     console.log(vocalText);
-    // let type = textArr[0];
-    // let position = textArr[2];
     let type = vocalText.match(/([가-힣]+\s[가-힣]*)/g)[0];
     let position = vocalText.match((/\d+(\s\d+)*/g))[0];
     console.log(type);
     console.log(position);
-    console.log(vocalText);
-=======
-    const textArr = vocalText.split(" ");
-
-    console.log(vocalText);
-    let type = textArr[0];
-    let position = textArr[2];
-
->>>>>>> Stashed changes
     document.getElementById('information').innerHTML = type + position;
 
 
